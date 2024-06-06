23145623c3c360bb72a0a85841c7739f9a9bc081
student <grechishnikovdanil@yandex.ru>
2024-06-06 14:47:42 +0300

    newcom

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import javax.swing.;
+import javax.swing.*;
 
-public class Main {
+public class main {
+
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        System.out.println("Hello world");
-
-        JFrame f = new JFrame(title; "");
+        JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        MyPanel myPanel = new MyPanel();
+        mypanel myPanel = new mypanel();
         myPanel.repaint();
+        myPanel.setSymbols("1,2,3");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,73 +1,84 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util. ArrayList;
 
-public class MyPanel extends JPanel {
-    public MyPanel(){
-
+public class mypanel extends JPanel {
+    public mypanel() {
         setBorder(BorderFactory.createLineBorder(Color.black));
     }
-
-    public Dimension getPreferredSize(){
-        return new Dimension(1560, 700);
-    }
-
+    public Dimension getPreferredSize() { return new Dimension(1560, 700); }
     private int x = 100;
-
     private int y = 100;
-
-    private int width = 200;
-
-    private int height = 200;
-
-    private int delta = 10;
-
+    private int width = 50;
+    private int height = 100;
+    private int step = 30;
+    private int delta = 5;
     private ArrayList<Rectangle> outlist = new ArrayList<Rectangle>();
-
     public void setSymbols(String parm) {
-        segment(1);
-        segment(2);
-        segment(3);
-        segment(4);
-        segment(5);
-        segment(6);
-        segment(7);
+        String[] arr = parm.replace(",", "").split("");
+        for (String cifra : arr) {
+            switch (cifra) {
 
+                case "1":
+                    segment(2);
+                    segment(4);
+                    break;
+                case "2":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(5);
+                    segment(6);
+                    break;
+                case "3":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    break;
+
+            }
+            x = x + step + width;
+        }
     }
-
-    private void segment(int number) {
-        switch (number) {
-            case 1:
-                outlist.add(new Rectangle(x + delta, y, width - delta, y));
+    private void segment(int number){
+        switch(number){
+            case 1:outlist.add(new Rectangle(x+delta,y, x+width-delta,y));
                 break;
-            case 2:
-                outlist.add(new Rectangle(width, y + delta, width, y + (height / 2) - delta));
+            case 2:outlist.add(new Rectangle(x+width, y+delta, x+width, y+(height/2)-delta));
                 break;
-            case 3:
-                outlist.add(new Rectangle(x + delta, y + (height / 2), width - delta, y + height / 2));
+            case 3:outlist.add(new Rectangle(x+delta, y+height/2, x+width-delta, y+height/2));
                 break;
-            case 4:
-                outlist.add(new Rectangle(width, y + (height / 2) + delta, width, y + height - delta));
+            case 4:outlist.add(new Rectangle(x+width, y+(height/2)+delta, x+width, y+height-delta));
                 break;
-            case 5:
-                outlist.add(new Rectangle(x + delta, y + delta, width - delta, y + height));
+            case 5:outlist.add(new Rectangle(x+delta, y+height, x+width-delta, y+height));
                 break;
-            case 6:
-                outlist.add(new Rectangle(x, y + (height / 2) + delta, x, y + height - delta));
+            case 6:outlist.add(new Rectangle(x, y+(height/2)+delta, x, y+height-delta));
                 break;
-            case 7:
-                outlist.add(new Rectangle(x, y + delta, x, y + (height / 2) - delta));
+            case 7:outlist.add(new Rectangle(x, y+delta, x, y+(height/2)-delta));
+                break;
+            case 8:outlist.add(new Rectangle(x+width, y+height, x+width+delta*2,y+height));
+                break;
+            case 9:outlist.add(new Rectangle(x+width+delta*2, y+height, x+width+delta*2,y+height+delta*2));
+                break;
+            case 10:outlist.add(new Rectangle(x, y+height, x-delta*2,y+height));
+                break;
+            case 11:outlist.add(new Rectangle(x-delta*2, y+height, x-delta*2,y+height+delta*2));
+                break;
+            case 12:outlist.add(new Rectangle(x+width/2-delta, y+height/2-delta, x+delta, y+delta));
+                break;
+            case 13:outlist.add(new Rectangle(x+width/2+delta/2, y+height/2-delta, x+width-delta, y+delta));
+                break;
+            case 14:outlist.add(new Rectangle(x+delta, y+height-delta, x+width/2-delta/2, y+height/2+delta));
                 break;
         }
     }
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for (Rectangle rect: outlist) {
+        for (Rectangle rect:outlist) {
             g.drawLine(rect.x, rect.y, rect.width, rect.height);
         }
     }
 }
-
